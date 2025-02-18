9441636832cf3ad28536b4d97046e4d6f74b5c9d
SJKZ1-2565 <seefuture13@gmail.com>
2025-02-18 19:00:42 +0700

    Update to 1.21.4

--- common/src/main/java/com/sjkz1/showkeybinds/mixin/MixinGuiGraphics.java
+++ common/src/main/java/com/sjkz1/showkeybinds/mixin/MixinGuiGraphics.java
@@ -12,7 +12,7 @@ import java.awt.*;
 
 @Mixin(GuiGraphics.class)
 public class MixinGuiGraphics {
-    @ModifyArgs(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I"))
+    @ModifyArgs(method = "renderItemCount", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I"))
     public void renderGuiItemDecorations$Colored(Args args) {
         int rainbow = Math.abs(Color.HSBtoRGB(System.currentTimeMillis() % 2500L / 2500F, 0.8F, 0.8F));
         var itemColor = Showkeybinds.CONFIG.general.rainBowItemText ? rainbow : Showkeybinds.CONFIG.general.itemCountColor;
