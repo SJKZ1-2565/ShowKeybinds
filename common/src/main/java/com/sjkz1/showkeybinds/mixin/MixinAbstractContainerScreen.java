da87bd99ecd11995abaabccd808fd1fecbdeae1b
SJKZ1-2565 <seefuture13@gmail.com>
2025-12-22 12:56:11 +0700

    chore: use matrix4fstack and styled code

--- common/src/main/java/com/sjkz1/showkeybinds/mixin/MixinAbstractContainerScreen.java
+++ common/src/main/java/com/sjkz1/showkeybinds/mixin/MixinAbstractContainerScreen.java
@@ -1,6 +1,8 @@
 package com.sjkz1.showkeybinds.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.sjkz1.showkeybinds.Showkeybinds;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
@@ -8,9 +10,11 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.client.gui.screens.inventory.MerchantScreen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Items;
+import org.joml.Matrix4fStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,32 +31,33 @@ public abstract class MixinAbstractContainerScreen<T extends AbstractContainerMe
     @Inject(method = "renderSlot", at = @At(value = "TAIL"))
     public void renderSlot(GuiGraphics guiGraphics, Slot slot, CallbackInfo ci) {
         if (Showkeybinds.CONFIG.container.enableContainerText) {
-            guiGraphics.pose().pushMatrix();
-            guiGraphics.pose().translate(0f, 0f);
+            Screen screen = Minecraft.getInstance().screen;
+            KeyMapping[] keyMappingList = Minecraft.getInstance().options.keyHotbarSlots;
+            Matrix4fStack stack = RenderSystem.getModelViewStack();
             float scale = Showkeybinds.CONFIG.container.containerScale;
-            guiGraphics.pose().scale(scale, scale);
-
-            var list = Minecraft.getInstance().options.keyHotbarSlots;
-            int textY = slot.getItem().is(Items.LIGHT) ? 8 : 0;
-            var screen = Minecraft.getInstance().screen;
+            int textYOffsets = slot.getItem().is(Items.LIGHT) ? 8 : 0;
             int rainbow = Math.abs(Color.HSBtoRGB(System.currentTimeMillis() % 2500L / 2500F, 0.8F, 0.8F));
-            var containerColor = Showkeybinds.CONFIG.container.rainBowText ? rainbow : Showkeybinds.CONFIG.container.containerTextColor;
-            var showOffHandText = Showkeybinds.CONFIG.general.offHandText;
+            int containerColor = Showkeybinds.CONFIG.container.rainBowText ? rainbow : Showkeybinds.CONFIG.container.containerTextColor;
+            boolean showOffHandText = Showkeybinds.CONFIG.general.offHandText;
             boolean isCreativeOrMerchantScreen = screen instanceof CreativeModeInventoryScreen || screen instanceof MerchantScreen;
             boolean isSpecialSlotY = isCreativeOrMerchantScreen ? (slot.y == 112 || slot.y == 142 || slot.y == 20) : (slot.y == 142 || slot.y == 143 || slot.y == 197 || slot.y == 109 || slot.y == 195);
 
+            stack.pushMatrix();
+            stack.translate(0f, 0f, 350f);
+            stack.scale(scale, scale, scale);
+
             if (isSpecialSlotY) {
-                int[] xCoordinates = isCreativeOrMerchantScreen
+                int[] slotX = isCreativeOrMerchantScreen
                         ? new int[]{9, 27, 45, 63, 81, 99, 117, 135, 153, 35}
                         : new int[]{8, 26, 44, 62, 80, 98, 116, 134, 152};
 
-                for (int i = 0; i < xCoordinates.length; i++) {
-                    if (slot.x == xCoordinates[i]) {
-                        var keyMessage = i < 9 ? list[i].getTranslatedKeyMessage() : Minecraft.getInstance().options.keySwapOffhand.getTranslatedKeyMessage();
+                for (int index = 0; index < slotX.length; index++) {
+                    if (slot.x == slotX[index]) {
+                        Component keyMessage = index < 9 ? keyMappingList[index].getTranslatedKeyMessage() : Minecraft.getInstance().options.keySwapOffhand.getTranslatedKeyMessage();
                         guiGraphics.drawString(this.font,
                                 keyMessage,
                                 (int) (slot.x / scale),
-                                (int) ((slot.y) / scale) + textY,
+                                (int) ((slot.y) / scale) + textYOffsets,
                                 containerColor,
                                 Showkeybinds.CONFIG.container.shadowedText);
                     }
@@ -60,11 +65,11 @@ public abstract class MixinAbstractContainerScreen<T extends AbstractContainerMe
             }
             //This fixed off-hand key render when it's survival inventory
             if (slot.x == 77 && slot.y == 62 && showOffHandText) {
-                var keyMessage = Minecraft.getInstance().options.keySwapOffhand.getTranslatedKeyMessage();
+                Component keyMessage = Minecraft.getInstance().options.keySwapOffhand.getTranslatedKeyMessage();
                 guiGraphics.drawString(this.font,
                         keyMessage,
                         (int) (slot.x / scale),
-                        (int) ((slot.y) / scale) + textY,
+                        (int) ((slot.y) / scale) + textYOffsets,
                         containerColor,
                         Showkeybinds.CONFIG.container.shadowedText);
             }
@@ -72,12 +77,12 @@ public abstract class MixinAbstractContainerScreen<T extends AbstractContainerMe
                 guiGraphics.drawString(this.font,
                         String.valueOf(slot.x),
                         (int) (slot.x / scale),
-                        (int) ((slot.y) / scale) + textY,
+                        (int) ((slot.y) / scale) + textYOffsets,
                         containerColor,
                         Showkeybinds.CONFIG.container.shadowedText);
             }
 
-            guiGraphics.pose().popMatrix();
+            stack.popMatrix();
         }
 
     }
